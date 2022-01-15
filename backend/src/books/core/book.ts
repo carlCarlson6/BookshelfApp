ddb926af74d34b0a53eafa32895d42a1744cc6be
Carlos Acitores Deval <carlos.acitores@colba.es>
2022-01-15 18:57:12 +0100

    wip

---
+++ backend/src/books/core/book.ts
@@ -0,0 +1,8 @@
+export class Book {
+    constructor(
+        readonly Id: string,
+        readonly Title: string,
+        readonly Author: string,
+        readonly Location: string,
+    ) {}
+}
