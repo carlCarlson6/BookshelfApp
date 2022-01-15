ffc7cebf5ccf128923d7d67b5e750c575962f91d
Carlos Acitores Deval <carlos.acitores@colba.es>
2022-01-15 18:57:55 +0100

    delete all backend

--- backend/src/books/core/book.ts
+++
@@ -1,8 +0,0 @@
-export class Book {
-    constructor(
-        readonly Id: string,
-        readonly Title: string,
-        readonly Author: string,
-        readonly Location: string,
-    ) {}
-}
--- backend/src/books/core/location.ts
+++
@@ -1,4 +0,0 @@
-export enum Location {
-    Valencia,
-    Losa
-}
